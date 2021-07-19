5febf3853e8d19bc13eec094d45dcfb960ef3784
michaelskyba <michaelskyba1411@gmail.com>
2021-07-19 02:17:18 -0400

    learning go: basic input

--- bpass.go
+++ bpass.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
 
 func main() {
-	version := 0.1
-	fmt.Println("Welcome to BudgetPass version", version)
+	version := "0.1.1"
+	stable := false
+
+	fmt.Printf("Welcome to BudgetPass version %v. To say that it's stable would be %v.\n", version, stable)
+	fmt.Printf("Enter a command (help): ")
+
+	// Get input
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	input := scanner.Text()
+	fmt.Printf("command not found: %v\n", input)
 }
