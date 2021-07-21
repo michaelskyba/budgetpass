a68cc99c91567440838bcc513d4b3ed0edd2d029
michaelskyba <michaelskyba1411@gmail.com>
2021-07-21 03:20:19 -0400

    cleaning

--- bpass.go
+++ bpass.go
@@ -5,14 +5,12 @@ import (
 	"os"
 	// "crypto/aes"
 	// "crypto/cipher"
-	// "io/ioutil"
 )
 
 func main() {
 	// Exit if no command
 	// os.Args includes "bpass", so we check if 1, not 0
 	if len(os.Args) == 1 {
-	fmt.Println("see 'bpass help' for usage instructions")
 		os.Exit(1)
 	}
 
@@ -33,21 +31,17 @@ func main() {
 			fmt.Printf("Enter the name of this password: ")
 			fmt.Scanln(&password_name)
 		}
+
+		// Get main input
 		fmt.Printf("Enter your master password: ")
 		fmt.Scanln(&master_password)
 
 		fmt.Printf("Enter the password for '%v': ", password_name)
 		fmt.Scanln(&local_password)
 
-	// Help
-	case "help":
-		fmt.Println("currently implemented commands:")
-		fmt.Println("- help")
-		fmt.Println("- new \"[password name]\"")
-
 	// Command not found
 	default:
-		fmt.Printf("command not found: %v\n", args)
+		os.Exit(1)
 
 	}
 }
